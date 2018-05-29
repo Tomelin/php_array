7d95e006126b532b89360e1193220e62b7c36325
Tomelin <rafael.tomelin@gmail.com>
2018-05-29 16:40:04 -0300

    Create web-service.php

---
+++ web-service.php
@@ -0,0 +1,41 @@
+<?php
+/* require the user as the parameter */
+if(isset($_GET['user']) && intval($_GET['user'])) {
+
+	/* soak in the passed variable or set our own */
+	$number_of_posts = isset($_GET['num']) ? intval($_GET['num']) : 10; //10 is the default
+	$format = strtolower($_GET['format']) == 'json' ? 'json' : 'xml'; //xml is the default
+	$user_id = intval($_GET['user']); //no default
+
+  /* create array */
+	$posts = array(
+		"num" => $number_of_posts,
+		"user" => $user_id,
+	);
+
+	/* output in necessary format */
+	if($format == 'json') {
+		header('Content-type: application/json');
+		echo json_encode(array('posts'=>$posts));
+	}
+	else {
+		header('Content-type: text/xml');
+		echo '<posts>';
+		foreach($posts as $index => $post) {
+			if(is_array($post)) {
+				foreach($post as $key => $value) {
+					echo '<',$key,'>';
+					if(is_array($value)) {
+						foreach($value as $tag => $val) {
+							echo '<',$tag,'>',htmlentities($val),'</',$tag,'>';
+						}
+					}
+					echo '</',$key,'>';
+				}
+			}
+		}
+		echo '</posts>';
+	}
+}
+
+?>
