73f37440506b95980ef9cc4190d5773f906d8f62
Rafael Tomelin <rafaeltomelin@tomelin.local>
2018-05-29 22:04:56 -0300

    Teste 2 atom

--- web-service.php
+++ web-service.php
@@ -3,7 +3,7 @@
 if(isset($_GET['domain']) && isset($_GET['software']) && isset($_GET['version'])) {
 
 	/* soak in the passed variable or set our own */
-	$version = $_GET['version'];   //0 is the default
+	$version = $_GET['version'];  
 	$format = strtolower($_GET['format']) == 'json' ? 'json' : 'xml'; //xml is the default
 	$software = $_GET['software']; //no default
 	$domain = $_GET['domain']; //no default
@@ -39,5 +39,4 @@ if(isset($_GET['domain']) && isset($_GET['software']) && isset($_GET['version'])
 		echo '</posts>';
 	}
 }
-
 ?>
